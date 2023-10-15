bc9c2209cd4da03f2b695dea59554d9f6d630c57
nomnomnonono <murumuru1204@gmail.com>
2023-10-15 20:57:41 +0900

    set env as production

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -2,4 +2,8 @@
 
 /cloud_sql_proxy -instances=$GCP_PROJECT_ID:$LOCATION:$CLOUD_SQL_INSTANCE_NAME=tcp:3306 &
 
+RUN bundle exec rake assets:precompile
+bin/rails db:migrate RAILS_ENV=production
+bin/rails db:seed RAILS_ENV=production
+
 exec "$@"
