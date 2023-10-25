6719a622552a1df8dbd2d3f9887c5676c4f47991
nomnomnonono <murumuru1204@gmail.com>
2023-10-25 19:16:45 +0900

    debug deploy

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
-/cloud_sql_proxy -instances=$GCP_PROJECT_ID:$LOCATION:$CLOUD_SQL_INSTANCE_NAME=tcp:3306 &
-
-RUN bundle exec rake assets:precompile
-bin/rails db:migrate RAILS_ENV=production
-bin/rails db:seed RAILS_ENV=production
+bundle exec rake assets:precompile
+#bin/rails db:migrate RAILS_ENV=production
+#bin/rails db:seed RAILS_ENV=production
 
 exec "$@"
